8a5fe84e5dd1c5017f604e159819bd154b436433
bwynants <bwynants+github@gmail.com>
2023-12-18 19:03:41 +0100

    valudate user in limits

--- lib/scale/BS444Scale.h
+++ lib/scale/BS444Scale.h
@@ -40,6 +40,7 @@
  */
 static const uint8_t kMaxUsers = 2;
 static const uint8_t kSleepTimeBetweenScans = 30;
+static const char *TAG = "BS444Scale";
 
 
 // https://www.leef.nl/kennisbank/weegschalen-en-vetmeters
@@ -105,7 +106,7 @@ class BS444Scale : public Component {
         // We have found a device, let us now see if it contains the service we are looking for.
         if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(mServiceUUID))
         {
-          ESP_LOGD("BS444","BLE Advertised Device found with our service%s", advertisedDevice.toString().c_str());
+          ESP_LOGD(TAG,"BLE Advertised Device found with our service%s", advertisedDevice.toString().c_str());
           BLEDevice::getScan()->stop(); //we found our device
           scaleDevice = new BLEAdvertisedDevice(advertisedDevice);
           mDoConnect = true;
@@ -164,18 +165,18 @@ class BS444Scale : public Component {
           size_t length,
           bool isNotify)
       {
-        ESP_LOGD("BS444","Notify callback for characteristic %s of data length %d data:", pBLERemoteCharacteristic->getUUID().toString().c_str(), pBLERemoteCharacteristic->getHandle(), length);
+        ESP_LOGD(TAG,"Notify callback for characteristic %s of data length %d data:", pBLERemoteCharacteristic->getUUID().toString().c_str(), pBLERemoteCharacteristic->getHandle(), length);
         if (pBLERemoteCharacteristic->getUUID().equals(Char_person))
         {
           mPerson = Person::decode(pData);
-          ESP_LOGD("BS444","data %s:", mPerson.toString().c_str());
+          ESP_LOGD(TAG,"data %s:", mPerson.toString().c_str());
         }
         else if (pBLERemoteCharacteristic->getUUID().equals(Char_weight))
         {
           auto data = Weight::decode(pData);
           if (data.timestamp < now())
           {
-            ESP_LOGD("BS444","data %s:", data.toString().c_str());
+            ESP_LOGD(TAG,"data %s:", data.toString().c_str());
             if (!mWeight.valid || (mWeight < data))
               mWeight = data;
           }
@@ -185,7 +186,7 @@ class BS444Scale : public Component {
           auto data = Body::decode(pData);
           if (data.timestamp < now())
           {
-            ESP_LOGD("BS444","data %s:", data.toString().c_str());
+            ESP_LOGD(TAG,"data %s:", data.toString().c_str());
             if (!mBody.valid || (mBody < data))
               mBody = data;
           }
@@ -195,23 +196,26 @@ class BS444Scale : public Component {
   
       void onDisconnect(BLEClient *pclient)
       {
-        ESP_LOGD("BS444","onDisconnect");
-        if (mPerson.valid)
-        {
-          ESP_LOGD("BS444","person %s:", mPerson.toString().c_str());
-          if (mWeight.valid && (mWeight.person == mPerson.person)){
-            ESP_LOGD("BS444","%s:", mWeight.toString(mPerson).c_str());
-            this->sensor_weights[mWeight.person-1]->publish_state(mWeight.weight);
-            this->sensor_bmi[mWeight.person-1]->publish_state(mWeight.weight / (mPerson.size * mPerson.size));
+        ESP_LOGD(TAG,"onDisconnect");
+        if (mPerson.valid) {
+          if ((mPerson.person >= 1) && (mPerson.person <= kMaxUsers)) {
+            ESP_LOGD(TAG,"person %s:", mPerson.toString().c_str());
+            if (mWeight.valid && (mWeight.person == mPerson.person)){
+              ESP_LOGD(TAG,"%s:", mWeight.toString(mPerson).c_str());
+              this->sensor_weights[mWeight.person-1]->publish_state(mWeight.weight);
+              this->sensor_bmi[mWeight.person-1]->publish_state(mWeight.weight / (mPerson.size * mPerson.size));
+            }
+            if (mBody.valid && (mBody.person == mPerson.person)){
+              ESP_LOGD(TAG,"%s:", mBody.toString().c_str());
+              this->sensor_kcal[mBody.person-1]->publish_state(mBody.kcal);
+              this->sensor_fat[mBody.person-1]->publish_state(mBody.fat);
+              this->sensor_tbw[mBody.person-1]->publish_state(mBody.tbw);
+              this->sensor_muscle[mBody.person-1]->publish_state(mBody.muscle);
+              this->sensor_bone[mBody.person-1]->publish_state(mBody.bone);
+            }
           }
-          if (mBody.valid && (mBody.person == mPerson.person)){
-            ESP_LOGD("BS444","%s:", mBody.toString().c_str());
-            this->sensor_kcal[mBody.person-1]->publish_state(mBody.kcal);
-            this->sensor_fat[mBody.person-1]->publish_state(mBody.fat);
-            this->sensor_tbw[mBody.person-1]->publish_state(mBody.tbw);
-            this->sensor_muscle[mBody.person-1]->publish_state(mBody.muscle);
-            this->sensor_bone[mBody.person-1]->publish_state(mBody.bone);
-          }
+          else
+            ESP_LOGE(TAG,"User not defined %d:", mPerson.person);
         }
 
         mConnected = false;
@@ -223,11 +227,11 @@ class BS444Scale : public Component {
       {
         //scaleDevice is a variable containing information about the device to be connected.
         //In the device scan below, information will be entered into the corresponding variable.
-        ESP_LOGD("BS444","Creating a connection to %s", scaleDevice->getAddress().toString().c_str());
+        ESP_LOGD(TAG,"Creating a connection to %s", scaleDevice->getAddress().toString().c_str());
 
         //Create a client (Central) class to connect to the server (Pheriphral)
         BLEClient *pClient = BLEDevice::createClient();
-        ESP_LOGI("BS444","Created client");
+        ESP_LOGI(TAG,"Created client");
 
         class MyClientCallback : public BLEClientCallbacks
         {
@@ -252,13 +256,13 @@ class BS444Scale : public Component {
 
         //Finally tried to connect to the server (Pheriphral) device!!!
         pClient->connect(scaleDevice); // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-        ESP_LOGI("BS444","Connected to server");
+        ESP_LOGI(TAG,"Connected to server");
         // set client to request maximum MTU from server (default is 23 otherwise)
         pClient->setMTU(517); 
 
         //Receive service information when connected
         for (auto service: *pClient->getServices()){
-           ESP_LOGD("BS444","service:  %s, handle %d", service.first.c_str(), service.second->getHandle());
+           ESP_LOGD(TAG,"service:  %s, handle %d", service.first.c_str(), service.second->getHandle());
         }
 
         //
@@ -267,14 +271,14 @@ class BS444Scale : public Component {
         BLERemoteService *pRemoteService = pClient->getService(mServiceUUID);
         if (pRemoteService == nullptr)
         {
-          ESP_LOGE("BS444","Failed to find our service UUID: %s", mServiceUUID.toString().c_str());
+          ESP_LOGE(TAG,"Failed to find our service UUID: %s", mServiceUUID.toString().c_str());
           pClient->disconnect();
           return false;
         }
-        ESP_LOGD("BS444","service %s found", mServiceUUID.toString().c_str());
+        ESP_LOGD(TAG,"service %s found", mServiceUUID.toString().c_str());
 
         for (auto characteristic: *pRemoteService->getCharacteristics()){
-           ESP_LOGD("BS444","characteristic:  %s -> handle %d", characteristic.first.c_str(), characteristic.second->getHandle());
+           ESP_LOGD(TAG,"characteristic:  %s -> handle %d", characteristic.first.c_str(), characteristic.second->getHandle());
         }
         //
         // subscribe to characteristics
@@ -284,53 +288,53 @@ class BS444Scale : public Component {
           auto pRemoteCharacteristic = pRemoteService->getCharacteristic(characteristic);
           if (pRemoteCharacteristic == nullptr)
           {
-            ESP_LOGE("BS444","Failed to find our characteristic UUID: %s", characteristic.toString().c_str());
+            ESP_LOGE(TAG,"Failed to find our characteristic UUID: %s", characteristic.toString().c_str());
             pClient->disconnect();
           }
-          ESP_LOGD("BS444","Found characteristic: Handle: %d, UUID: %s", pRemoteCharacteristic->getHandle(), characteristic.toString().c_str());
+          ESP_LOGD(TAG,"Found characteristic: Handle: %d, UUID: %s", pRemoteCharacteristic->getHandle(), characteristic.toString().c_str());
 
           // Read the value of the characteristic.
           if (pRemoteCharacteristic->canRead())
           {
-            ESP_LOGD("BS444","The characteristic canRead");
+            ESP_LOGD(TAG,"The characteristic canRead");
             std::string value = pRemoteCharacteristic->readValue();
-            ESP_LOGD("BS444","The characteristic value was: %s", value.c_str());
+            ESP_LOGD(TAG,"The characteristic value was: %s", value.c_str());
           }
 
           if (pRemoteCharacteristic->canNotify())
           {
-            ESP_LOGD("BS444","The characteristic canNotify");
+            ESP_LOGD(TAG,"The characteristic canNotify");
             pRemoteCharacteristic->registerForNotify(std::bind (&BS444Scale::notifyCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4) );
-            ESP_LOGD("BS444","Registered Callback");
+            ESP_LOGD(TAG,"Registered Callback");
           }
 
           if (pRemoteCharacteristic->canIndicate())
           {
-            ESP_LOGD("BS444","The characteristic canIndicate");
+            ESP_LOGD(TAG,"The characteristic canIndicate");
             pRemoteCharacteristic->registerForNotify(std::bind (&BS444Scale::notifyCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4) );
-            ESP_LOGD("BS444","Registered Callback");
+            ESP_LOGD(TAG,"Registered Callback");
 
             for (auto descriptor: *pRemoteCharacteristic->getDescriptors()){
-              ESP_LOGD("BS444","descriptor:  %s, handle %d", descriptor.first.c_str(), descriptor.second->getHandle());
+              ESP_LOGD(TAG,"descriptor:  %s, handle %d", descriptor.first.c_str(), descriptor.second->getHandle());
             }
             // do we find a descriptor with a handle of the pRemoteCharacteristic + 1 ?
             auto it = std::find_if(pRemoteCharacteristic->getDescriptors()->begin(), pRemoteCharacteristic->getDescriptors()->end(), [&pRemoteCharacteristic](std::map<std::string, BLERemoteDescriptor *>::value_type &descriptor)
                                    { return (descriptor.second->getHandle() == pRemoteCharacteristic->getHandle() + 1); });
             if (it != pRemoteCharacteristic->getDescriptors()->end())
             {
-              ESP_LOGD("BS444","descriptor found:  %s, handle %d", it->first.c_str(), it->second->getHandle());
+              ESP_LOGD(TAG,"descriptor found:  %s, handle %d", it->first.c_str(), it->second->getHandle());
               const uint8_t indicationOn[] = {0x2, 0x0};
               it->second->writeValue((uint8_t *)indicationOn, 2, true);
-              ESP_LOGD("BS444","Indication Enabled");
+              ESP_LOGD(TAG,"Indication Enabled");
             }
           }
         }
         auto pRemoteCharacteristic = pRemoteService->getCharacteristic(Char_command);
         if (pRemoteCharacteristic != nullptr)
         {
-          ESP_LOGI("BS444","Found characteristic: Handle: %d, UUID: %s", pRemoteCharacteristic->getHandle(), Char_command.toString().c_str());
+          ESP_LOGI(TAG,"Found characteristic: Handle: %d, UUID: %s", pRemoteCharacteristic->getHandle(), Char_command.toString().c_str());
           if(pRemoteCharacteristic->canWrite()) {
-            ESP_LOGD("BS444","The characteristic canWrite");
+            ESP_LOGD(TAG,"The characteristic canWrite");
 
             /*
             Send the unix timestamp in little endian order preceded by 02 as
@@ -345,7 +349,7 @@ class BS444Scale : public Component {
           }
           else
           {
-            ESP_LOGE("BS444","Can not write to characteristic UUID: %s", Char_command.toString().c_str());
+            ESP_LOGE(TAG,"Can not write to characteristic UUID: %s", Char_command.toString().c_str());
             pClient->disconnect();
           }
         }
@@ -363,9 +367,9 @@ class BS444Scale : public Component {
         if (mDoConnect)
         {
           if (connectToServer())
-            ESP_LOGI("BS444","We are now connected to the BLE Server.");
+            ESP_LOGI(TAG,"We are now connected to the BLE Server.");
           else
-            ESP_LOGE("BS444","We have failed to connect to the server; there is nothing more we will do.");
+            ESP_LOGE(TAG,"We have failed to connect to the server; there is nothing more we will do.");
           mDoConnect = false;
         }
 
@@ -377,7 +381,7 @@ class BS444Scale : public Component {
         }
         else if (mDoScan)
         {
-           ESP_LOGI("BS444","starting scan in 30 seconds.");
+           ESP_LOGI(TAG,"starting scan in 30 seconds.");
            //start a next scan in 30 time
            mDoScan = false;
            mScan = true;
@@ -389,7 +393,7 @@ class BS444Scale : public Component {
           mScan = false;
           mDoScan = true; // restarting a delayed scan whenever possible
            //If the connection is released and mDoScan is true, start scanning
-          ESP_LOGI("BS444","Restarting scan.");
+          ESP_LOGI(TAG,"Restarting scan.");
           BLEDevice::getScan()->start(1, false);
         }
       }
